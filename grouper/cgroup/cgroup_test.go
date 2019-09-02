cd32c36cdce34cf648553b58201fbe4d405cb976
k1LoW <k1lowxb@gmail.com>
2019-09-02 17:38:20 +0900

    Add test

--- grouper/cgroup/cgroup_test.go
+++ grouper/cgroup/cgroup_test.go
@@ -9,13 +9,12 @@ import (
 )
 
 const (
-	cgroupTestProcPath   = "../../testdata/proc"
-	cgroupTestCgroupPath = "../../testdata/sys/fs/cgroup"
+	testProcPath   = "../../testdata/proc"
+	testCgroupPath = "../../testdata/sys/fs/cgroup"
 )
 
 func TestCollect(t *testing.T) {
-	os.Setenv("GROUPED_PROCESS_PROC_MOUNT_POINT", cgroupTestProcPath)
-	cgroup := NewCgroup(cgroupTestCgroupPath)
+	cgroup := testCgroup()
 	gpMap := make(map[string]*grouped_proc.GroupedProc)
 	enabled := make(map[metric.MetricKey]bool)
 
@@ -35,4 +34,33 @@ func TestCollect(t *testing.T) {
 	if _, ok := gpMap["/system.slice/mysql.service"]; !ok {
 		t.Errorf("want %s, got none", "/system.slice/mysql.service")
 	}
+}
+
+func TestCollectWithNormalize(t *testing.T) {
+	cgroup := testCgroup()
+	err := cgroup.SetNormalizeRegexp("/(system.slice).+")
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	gpMap := make(map[string]*grouped_proc.GroupedProc)
+	enabled := make(map[metric.MetricKey]bool)
+
+	enabled[metric.ProcIO] = true
+	enabled[metric.ProcStat] = true
+	err = cgroup.Collect(gpMap, enabled)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	if len(gpMap) != 1 {
+		t.Errorf("want %d, got %d", 1, len(gpMap))
+	}
+	if _, ok := gpMap["system.slice"]; !ok {
+		t.Errorf("want %s, got none", "system.slice")
+	}
+}
+
+func testCgroup() *Cgroup {
+	os.Setenv("GROUPED_PROCESS_PROC_MOUNT_POINT", testProcPath)
+	return NewCgroup(testCgroupPath)
 }
--- grouper/proc_status_name/proc_status_name_test.go
+++ grouper/proc_status_name/proc_status_name_test.go
@@ -9,12 +9,11 @@ import (
 )
 
 const (
-	cgroupTestProcPath = "../../testdata/proc"
+	testProcPath = "../../testdata/proc"
 )
 
 func TestCollect(t *testing.T) {
-	os.Setenv("GROUPED_PROCESS_PROC_MOUNT_POINT", cgroupTestProcPath)
-	procStatusName := NewProcStatusName()
+	procStatusName := testProcStatusName()
 	gpMap := make(map[string]*grouped_proc.GroupedProc)
 	enabled := make(map[metric.MetricKey]bool)
 
@@ -34,4 +33,36 @@ func TestCollect(t *testing.T) {
 	if _, ok := gpMap["mysqld"]; !ok {
 		t.Errorf("want %s, got none", "mysqld")
 	}
+}
+
+func TestCollectWithNormalize(t *testing.T) {
+	procStatusName := testProcStatusName()
+	err := procStatusName.SetNormalizeRegexp("(mysql).+")
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	gpMap := make(map[string]*grouped_proc.GroupedProc)
+	enabled := make(map[metric.MetricKey]bool)
+
+	enabled[metric.ProcIO] = true
+	enabled[metric.ProcStat] = true
+	err = procStatusName.Collect(gpMap, enabled)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	if len(gpMap) != 2 {
+		t.Errorf("want %d, got %d", 2, len(gpMap))
+	}
+	if _, ok := gpMap["nginx"]; !ok {
+		t.Errorf("want %s, got none", "nginx")
+	}
+	if _, ok := gpMap["mysql"]; !ok {
+		t.Errorf("want %s, got none", "mysql")
+	}
+}
+
+func testProcStatusName() *ProcStatusName {
+	os.Setenv("GROUPED_PROCESS_PROC_MOUNT_POINT", testProcPath)
+	return NewProcStatusName()
 }
